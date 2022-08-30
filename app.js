a6ed660bda0d1f0500a86b403219168d74854a09
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2022-08-29 22:13:09 -0600

    seed db

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+const app = express();
+
+main()
+  .then(() => console.log("DATABASE CONNECTION OPEN"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/yelp-camp");
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
 });
 
 app.listen(3000, () => {
