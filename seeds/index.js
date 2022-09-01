1c8ebb639065905f9196bcf550f0873e211a926f
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2022-08-31 22:04:34 -0600

    basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo vero dignissimos possimus quibusdam. Omnis, nemo illum? Quia vitae aliquam eos sapiente, deserunt veniam, eum ipsum, iste reiciendis et nesciunt sint?",
+      price,
     });
     await camp.save();
   }
